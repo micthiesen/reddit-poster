76cf3ca35960356a7798a6e1e321ce3e77fe744a
MasterMic <micthiesen@gmail.com>
2013-05-15 01:02:05 -0700

    Scheduling now works, the bot is now functioning

--- rposter.py
+++ rposter.py
@@ -9,13 +9,12 @@ import json
 import praw
 import time
 
-# Constants
-RETRY_WAIT_TIME = 10  # Number of seconds to wait before retrying to submit an submission
-WEEKDAYS = {0: "Sunday", 1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday",
-            5: "Friday", 6: "Saturday"}
 
-# Global varibales
-submissions = []
+# Constants
+CHECK_INTERVAL = 10  # Number of seconds to wait between checking for submissions to post
+RETRY_WAIT_TIME = 10  # Number of seconds to wait before retrying to submit an submission
+WEEKDAYS = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday",
+            5: "Saturday", 6: "Sunday"}
 
 
 # A submission
@@ -42,18 +41,35 @@ class Submission(object):
             time.sleep(RETRY_WAIT_TIME)
             self.submit()
 
+    def in_interval(self, lower, upper):
+        lower_minutes = get_minutes(lower.tm_wday, lower.tm_hour, lower.tm_min)
+        upper_minutes = get_minutes(upper.tm_wday, upper.tm_hour, upper.tm_min)
+        sub_minutes = get_minutes(self.time.day, self.time.hour, self.time.minute)
 
+        if lower_minutes <= upper_minutes:
+            return sub_minutes > lower_minutes and sub_minutes <= upper_minutes
+        else:
+            return sub_minutes > lower_minutes or sub_minutes <= upper_minutes
+
+    def to_string(self):
+        return "\"" + self.title + "\" to be run every " + self.time.to_string()
+
+
+# A time object
 class Time(object):
     def __init__(self, day, hour, minute):
         # Ensure the time format is correct
         if day < 0 or day > 6:
-            print("[ ERROR ] 'day' must be an integer ranging from 0 to 6")
+            print("[ ERROR ] 'day' must be an integer ranging from 0 to 6, "
+                  + "check your config.json file")
             exit(1)
         if (hour < 0 or hour > 23):
-            print("[ ERROR ] 'hour' must be an integer ranging from 0 to 23")
+            print("[ ERROR ] 'hour' must be an integer ranging from 0 to 23, "
+                  + "check your config.json file")
             exit(1)
         if (minute < 0 or minute > 59):
-            print("[ ERROR ] 'minute' must be an integer ranging from 0 to 59")
+            print("[ ERROR ] 'minute' must be an integer ranging from 0 to 59, "
+                  + "check your config.json file")
             exit(1)
 
         self.day = day
@@ -64,6 +80,10 @@ class Time(object):
         return WEEKDAYS[self.day] + " at " + str(self.hour) + ":" + str(self.minute)
 
 
+def get_minutes(day, hour, minute):
+    return (day * 1440) + (hour * 60) + minute
+
+
 # This method is used when rposter.py is invoked from the command line
 def main():
     # Load the config file
@@ -71,15 +91,14 @@ def main():
     print("[ INFO ] Submission data loaded from config.json")
 
     # Initialize the submissions
-    global submissions
+    submissions = []
     for s in data:
         try:
             a_time = Time(s["time"]["day"], s["time"]["hour"], s["time"]["minute"])
             a_submission = Submission(s["subreddit"], s["username"], s["password"],
                                       s["title"], s["text"], a_time)
             submissions.append(a_submission)
-            print("[ INFO ] Parsed submission titled \"" + s["title"] + "\" to be run "
-                  "every " + a_time.to_string())
+            print("[ INFO ] Parsed submission titled " + a_submission.to_string())
         except KeyError as e:
             print("[ ERROR ] The key " + str(e) + " could not be found for a submission, "
                   + "check your config.json file")
@@ -87,6 +106,20 @@ def main():
 
     # Queue the submissions to be submitted at the appropriate time
     # We assume all submissions prior to this time have been submitted
+    # We also assume that all time intervals last less than one week
+    prev_time = time.localtime()
+    while True:
+        curr_time = time.localtime()
+        print("[ INFO ] Checking for submissions to submit (current time "
+              + str(curr_time.tm_hour) + ":" + str(curr_time.tm_min) + ":"
+              + str(curr_time.tm_sec) + ")")
+
+        for s in submissions:
+            if s.in_interval(prev_time, curr_time):
+                s.submit()
+
+        prev_time = curr_time
+        time.sleep(CHECK_INTERVAL)
 
 
 # Call main() when rposter.py is invoked from the command line
